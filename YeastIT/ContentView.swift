83bb37e562d75d00c9d084ff3b82445ca91419e9
Sabatino Cardillo <sabatino.cardillo@gmail.com>
2020-07-07 10:05:19 +0200

    commit

--- YeastIT/ContentView.swift
+++ YeastIT/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Ciao")
     }
 }
+
+//ciao
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
