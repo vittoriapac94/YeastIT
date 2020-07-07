b377fe73cdcaab57253f47862b1d1b6fb3e6ea8e
Vittoria <vittoriapacchiano@gmail.com>
2020-07-07 09:22:42 +0200

    Initial Commit

---
+++ YeastIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YeastIT
+//
+//  Created by Vittoria on 07/07/2020.
+//  Copyright © 2020 vittoria. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
