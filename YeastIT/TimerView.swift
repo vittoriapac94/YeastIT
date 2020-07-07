fe4e1383a8e2168e1a12b4672b2723ae3ba59130
Peppe3009 <ventrone524@gmail.com>
2020-07-07 18:24:38 +0200

    modifica timerView - Peppe

--- YeastIT/TimerView.swift
+++ YeastIT/TimerView.swift
@@ -14,6 +14,7 @@ struct TimerView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     let timerCL = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
        ZStack{
            Image("bg")
@@ -150,23 +151,28 @@ struct ProgressBar: View {
     @Binding var progress: Float
     
     var body: some View {
+
         ZStack {
             Circle()
-                .stroke(lineWidth: 20.0)
+                .stroke(lineWidth: 15.0)
                 .opacity(0.3)
                 .foregroundColor(Color.red)
             
             Circle()
                 .trim(from: 0.0, to: CGFloat(min(self.progress, 1.0)))
-                .stroke(style: StrokeStyle(lineWidth: 20.0, lineCap: .round, lineJoin: .round))
+                .stroke(style: StrokeStyle(lineWidth: 15.0, lineCap: .round, lineJoin: .round))
                 .foregroundColor(Color.red)
                 .rotationEffect(Angle(degrees: 270.0))
                 .animation(.linear)
             
-            Text(String(format: "%.0f %%", min(self.progress, 1.0)*100.0))
-                .font(.largeTitle)
-                .bold()
+            VStack{
+                Text("Mancano")
+                Text("23 ore")
+            }
+            
+            
         }
+        
     }
 }
 
