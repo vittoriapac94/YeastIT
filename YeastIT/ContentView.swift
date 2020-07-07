31c8ec3d04449b5f3662ea6b01383c1a19967548
Peppe3009 <ventrone524@gmail.com>
2020-07-07 16:12:07 +0200

    first view quasi finita - Peppe

--- YeastIT/ContentView.swift
+++ YeastIT/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Yeast IT
+//  YeastIt
 //
 //  Created by utente on 06/07/2020.
 //  Copyright © 2020 utente. All rights reserved.
@@ -12,78 +12,103 @@ struct ContentView: View {
     let textDimension : CGFloat = 40
     let imageDimension : CGFloat = 228
     let avatarImageDimension : CGFloat = 499
-    @State var quantity: String = ""
+    @State var quantity: String = "0"
     @State var avatarName: String = ""
-    
     var body: some View {
-        ZStack(alignment: .top){
-            Image("Background")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(.all)
-            VStack(alignment: .center){
-                HStack(alignment: .center) {
-                    Text("YEAST")
-                        .font(.system(size: textDimension, weight: .bold))
-                        .foregroundColor(Color.white)
-                        .multilineTextAlignment(.center)
-                        .font(.body)
-                    
-                    Text("IT")
-                        .font(.system(size: textDimension, weight: .light))
-                        .foregroundColor(Color.white)
-                        .multilineTextAlignment(.center)
-                    
-                }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                    .padding(20)
-                
-                ZStack(alignment: .center){
-                    Image("RettangoloAvatar")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: imageDimension * 1.2 , height: imageDimension )
-                        
-                        
-                    Image("img_avatar")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: avatarImageDimension / 3, height: avatarImageDimension / 3)
-                }.aspectRatio(contentMode: .fit)
-                
+        
+        NavigationView{
+            ZStack(alignment: .top){
+                Image("Background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
                 VStack(alignment: .center){
-                    
-                    TextField("Nome Avatar...",text: $avatarName)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .font(.system(size: textDimension / 2, weight: .light))
-                        .frame(width: avatarImageDimension / 2)
-
-                        .padding()
-                    
-                    
-                    TextField("Quantità...",text: $quantity)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .frame(width: avatarImageDimension / 2)
-                        .font(.system(size: textDimension / 2, weight: .light))
-                        .cornerRadius(10)
-                        .padding()
-                    
-                    
-                    
-                    Button(action: {
-                        print("tapped")
-                    }){
-                        Text("Ciao")
-                        .bold()
+                    HStack(alignment: .center) {
+                        Text("YEAST")
+                            .font(.system(size: textDimension, weight: .bold))
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.center)
+                        
+                        Text("IT")
+                            .font(.system(size: textDimension, weight: .light))
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.center)
                         
                     }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 10)
                     
-                }.padding(70)
+                    
+                    ZStack(alignment: .center){
+                        Image("RettangoloAvatar")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: imageDimension * 1.2 , height: imageDimension * 1.2 )
+                        
+                        
+                        Image("img_avatar")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: avatarImageDimension / 3, height: avatarImageDimension / 3)
+                            
+                    }.aspectRatio(contentMode: .fit)
+                        .padding()
+                    
+                    VStack(alignment: .center){
+                        VStack{
+                            Text("Scegli un nome per il tuo avatar")
+                            TextField("Nome Avatar...",text: $avatarName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .font(.system(size: textDimension / 2, weight: .light))
+                            .frame(width: avatarImageDimension / 2)
+                        }.padding()
+                        
+                        
+                        VStack{
+                            Text("Quantità di Lievito (gr)")
+                            TextField("Quantità...",text: $quantity)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .frame(width: avatarImageDimension / 2)
+                                .font(.system(size: textDimension / 2, weight: .light))
+                                .cornerRadius(10)
+                                .keyboardType(.numberPad)
+                                                  
+                        }
+                  
+                        
+                        
+                        ZStack{
+                            Image(uiImage: #imageLiteral(resourceName: "Casella Testo"))
+                                .resizable()
+                                .frame( height: 100)
+                                .padding()
+                            VStack(alignment: .center){
+                                Text("Ti servono")
+                                    .font(.system(size: 23, weight: .bold))
+                                Text("\(quantity) gr di farina")
+                                    .font(.system(size: 15, weight: .light))
+                                Text("\((Int(quantity) ?? 0) / 2) gr di acqua")
+                                    .font(.system(size: 15, weight: .light))
+                            }
+                        }
+                        
+                        NavigationLink(destination: TimerView()){
+                            ZStack{
+                                Rectangle()
+                                    .frame(width: 299, height: 60, alignment: .center)
+                                    .foregroundColor(Color.white)
+                                    .cornerRadius(25)
+                                Text("Inizia a lievitare")
+                                    .foregroundColor(Color.orange)
+                            }
+                        }
+                    }
+                    
+                }
             }
         }
     }
 }
 
-//buonasseeeeraaaaaaaa lorenzo
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -91,4 +116,4 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-//salve a tutti
+
---
+++ YeastIT/TimerView.swift
@@ -0,0 +1,21 @@
+//
+//  TimerView.swift
+//  YeastIT
+//
+//  Created by utente on 07/07/2020.
+//  Copyright © 2020 vittoria. All rights reserved.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
